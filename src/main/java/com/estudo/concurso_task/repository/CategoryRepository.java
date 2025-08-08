95dbbc4505e4fd904b543a062f12917ad31be567
jgnfps <cupreguiado@gmail.com>
2025-08-08 12:30:37 -0300

    feat: create entity category

---
+++ src/main/java/com/estudo/concurso_task/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.estudo.concurso_task.repository;
+
+import com.estudo.concurso_task.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
